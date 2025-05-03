8f253669af4736aa250c6f7f426cc73d90564f3d
Tarek <stktarekrahman@gmail.com>
2025-05-03 19:37:22 +0600

    Removed BrowserRouter

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,13 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
 const root = createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter basename="/revamping-old-react-blog">
-    <App />
-  </BrowserRouter>
-);
+root.render(<App />);
